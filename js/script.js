ea1ca91e996d0372ab7c1a58a126e1510cbd78cb
Gabriel-Amar <amariutei.gabriel1@gmail.com>
2022-04-20 16:26:51 +0200

    index, css e js: l'immagine sparisce

--- js/script.js
+++ js/script.js
@@ -2,12 +2,20 @@ const root = new Vue({
     el: "#root",
     data: {
         titolo: "Salve",
-        immagine: ""
+        immagine: "",
+        scrittaPulsante: "Mostra l'immagine"
     },
     methods:{
         mostraImmagine(){
-            this.titolo = "Ecco l'immagine"
-            this.immagine = "https://unsplash.it/600/300?image=171"
+            if(!this.immagine){
+                this.titolo = "Ecco l'immagine"
+                this.immagine = "https://unsplash.it/600/300?image=171"
+                this.scrittaPulsante = "Cancella l'immagine"
+            }else{
+                this.titolo = "L'immagine è sparita"
+                this.immagine = ""
+                this.scrittaPulsante = "Fai tornare l'immagine"
+            }
         }
     }
 })
